6e37af0bae291ee6e4635111ccd8f9dc1fc57499
Zach <zbayard@gmail.com>
2020-11-19 20:53:42 -0500

    finished

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,30 @@
+class Doctor
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select {|apt| apt.doctor == self}
+    end
+
+    def patients
+        appointments.map {|apt| apt.patient}
+    end
+
+
+
+end
 
